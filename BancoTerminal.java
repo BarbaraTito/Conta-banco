5264c8a08dc6ad49086cb4a6f31323aaed8419fa
Barbara <112973849+BarbaraTito@users.noreply.github.com>
2024-08-09 09:19:52 -0300

    Add files via upload

---
+++ BancoTerminal.java
@@ -0,0 +1,22 @@
+public class BancoTerminal{
+
+
+    public static void main (String[] args){
+        
+
+        // Nome do banco, com frase inicial
+        
+       //Agencia numeroAgencia = new Agencia(1021);
+        //Conta numeroConta = new Conta(067-8);
+
+
+
+         //Nome = "MARIO ANDRADE";
+         ///int numeroConta = 1021;
+         //String  conta = new Conta(067-8);
+         //double = 273.48
+    }
+
+
+}
+
