753280150325e1c96fb2c1604b834b65d9d486d7
Barbara <112973849+BarbaraTito@users.noreply.github.com>
2024-08-21 08:05:26 -0300

    Add files via upload

--- BancoTerminal.java
+++ BancoTerminal.java
@@ -1,12 +1,40 @@
 public class BancoTerminal{
 
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
+
+            //variavel da classe conta
+            
+                //Nome do Banco e mensagem de inicio
+                
+                String meu = "Novo";
+                String bancoSim="Povo";
+                String primeiroNome = "Mario";
+                String segundoNome = "Andrade";
+        
+                String nomeBanco = nomeBanco (meu,bancoSim);
+                System.out.print(nomeBanco);
+
+                String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+                System.out.print(nomeCompleto);
+                
+    }
+    
+            
+        public static String nomeBanco (String meu, String bancoSim){
+            return "Bem Vindo ao Banco "+ meu.concat(" ").concat(bancoSim);
+        } 
+        
+        public static String nomeCompleto (String primeiroNome, String segundoNome){
+            return " Cliente "  + primeiroNome.concat(" ").concat(segundoNome);
+        }
+            
+        
         
         
         
 
-        // Nome do banco, com frase inicial
+        // Nome do banco, com frase inicial ok
         
        //Agencia numeroAgencia = new Agencia(1021);
         //Conta numeroConta = new Conta(067-8);
@@ -17,7 +45,7 @@ public class BancoTerminal{
          ///int numeroConta = 1021;
          //String  conta = new Conta(067-8);
          //double = 273.48
-    }
+    
 
 
 }
